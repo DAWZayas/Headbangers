490552f2adbe7a16dd4982d3074e010ee9205ab7
Lester Pérez Santos <lester@MacBook-Pro-de-Lester.local>
2017-10-03 22:25:25 +0200

    Changed animation for small screens

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,10 @@ var menuOpened = false;
 function toggleMenu() {
     if (!menuOpened) {
         document.getElementById("side-menu").style.marginLeft = 0;
-        document.getElementsByTagName("main")[0].style.marginLeft = "14em";
+        if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = "14em";
     }else {
         document.getElementById("side-menu").style.marginLeft = "-14em";
-        document.getElementsByTagName("main")[0].style.marginLeft = 0;
+        if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = 0;
     }
     menuOpened = !menuOpened;
 }
