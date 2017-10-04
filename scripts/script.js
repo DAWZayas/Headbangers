f4c33f275486e859e39ce06fe86fb5d8b4173452
Lester Pérez Santos <lester@MacBook-Pro-de-Lester.local>
2017-10-04 10:27:44 +0200

    Changed function toggleMenu

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
-var menuOpened = false;
- 
-function toggleMenu() {
-    if (!menuOpened) {
-        document.getElementById("side-menu").style.marginLeft = 0;
-        if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = "14em";
-    }else {
-        document.getElementById("side-menu").style.marginLeft = "-14em";
-        if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = 0;
+const toggleMenu = (function (){
+    var menuOpened = false;
+    return function (){
+        if (!menuOpened) {
+            document.getElementById("side-menu").style.marginLeft = 0;
+            if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = "14em";
+        }else {
+            document.getElementById("side-menu").style.marginLeft = "-14em";
+            if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = 0;
+        }
+        menuOpened = !menuOpened;
     }
-    menuOpened = !menuOpened;
-}
+})();
