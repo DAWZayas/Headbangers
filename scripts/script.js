9a668792e70f0be00260ce4c30632fcc9c20dc97
Lester Pérez Santos <lester@MacBook-Pro-de-Lester.local>
2017-10-04 15:31:44 +0200

    Remove memu animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
-const toggleMenu = (function (){
+const toggleMenu = (function(){
     var menuOpened = false;
     return function (){
         if (!menuOpened) {
             document.getElementById("side-menu").style.marginLeft = 0;
-            if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = "14em";
         }else {
             document.getElementById("side-menu").style.marginLeft = "-14em";
-            if(window.innerWidth > 768)document.getElementsByTagName("main")[0].style.marginLeft = 0;
         }
         menuOpened = !menuOpened;
     }
