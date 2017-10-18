2bfac89b678d1018ad5a709fea7b3f45b96b979e
Lester Pérez Santos <lester@MacBook-Pro-de-Lester.local>
2017-10-18 23:27:11 +0200

    Added animation to side menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,66 @@
-const toggleMenu = (function(){
-    var menuOpened = false;
-    return function (){
-        if (!menuOpened) {
-            document.getElementById("side-menu").style.marginLeft = 0;
-        }else {
-            document.getElementById("side-menu").style.marginLeft = "-14em";
+const getWidth = () => document.body.clientWidth;
+
+const moveContentRight = () => {
+    document.getElementsByTagName("main")[0].style.marginLeft = "16em";
+    document.getElementsByTagName("main")[0].style.width = "calc(100% - 16em)";
+    document.getElementsByTagName("footer")[0].style.marginLeft = "16em";
+    document.getElementsByTagName("footer")[0].style.width = "calc(100% - 16em)";
+}
+
+const moveContentLeft = () => {
+    document.getElementsByTagName("main")[0].style.marginLeft = "0px";
+    document.getElementsByTagName("main")[0].style.width = "100%";
+    document.getElementsByTagName("footer")[0].style.marginLeft = "0px";
+    document.getElementsByTagName("footer")[0].style.width = "100%";
+}
+
+const openMenu = () => {
+    document.getElementById("side-menu").style.marginLeft = "0px";
+}
+
+const closeMenu = () => {
+    document.getElementById("side-menu").style.marginLeft = "-16em";
+}
+
+const openMenuPush = () => {
+    openMenu();
+    moveContentRight();
+}
+
+const closeMenuPush = () => {
+    closeMenu();
+    moveContentLeft();
+}
+
+const isMenuOpened = () => {
+    return document.getElementById("side-menu").style.marginLeft === "0px";
+}
+
+const toggleMenu = () => {
+    if (!isMenuOpened()) {
+        if (getWidth() < 768) {
+            openMenu();
+        } else {
+            openMenuPush();
         }
-        menuOpened = !menuOpened;
+    } else {
+        if (getWidth() < 768) {
+            closeMenu();
+        } else {
+            closeMenuPush();
+        }
     }
-})();
-document.getElementById("menu-button").addEventListener("click",toggleMenu);
+}
+
+const resized = () => {
+    if(getWidth() < 768 && isMenuOpened()) closeMenuPush();
+    else if(getWidth() > 768 && !isMenuOpened()) openMenuPush();
+}
+
+const init = () => {
+    document.getElementById("menu-button").addEventListener("click", toggleMenu);
+    window.addEventListener("resize", resized);
+    if(getWidth() > 768) openMenuPush();
+}
+
+init();
