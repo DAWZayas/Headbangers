29ba0f72ef57967df85ad3f0c96d1fde55d92920
Lester Pérez Santos <lester@MacBook-Pro-de-Lester.local>
2017-10-23 18:34:11 +0200

    Cleaner menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,15 +4,15 @@ const setMarginLeft = (element, margin) => element.style.marginLeft = margin;
 
 const setWidth = (element, width) => element.style.width = width;
 
-const moveContent = (margin, width) => {
+const moveWrapper = (margin, width) => {
     const wrapper = document.querySelector("#wrapper");
     setMarginLeft(wrapper, margin);
     setWidth(wrapper, width);
 }
 
-const moveContentRight = () => moveContent("16em", "calc(100% - 16em)");
+const moveWrapperRight = () => moveWrapper("16em", "calc(100% - 16em)");
 
-const moveContentLeft =() => moveContent("0px", "100%");
+const moveWrapperLeft =() => moveWrapper("0px", "100%");
 
 const moveMenu = (margin) => setMarginLeft(document.getElementById("side-menu"), margin);
 
@@ -20,33 +20,23 @@ const openMenu = () => moveMenu("0px");
 
 const closeMenu = () => moveMenu("-16em");
 
-function openMenuPush() {
+const openMenuPush = () => {
     openMenu();
-    moveContentRight();
+    moveWrapperRight();
 }
 
-function closeMenuPush() {
+const closeMenuPush = () => {
     closeMenu();
-    moveContentLeft();
+    moveWrapperLeft();
 }
 
-function isMenuOpened() {
-    return document.getElementById("side-menu").style.marginLeft === "0px";
-}
+const isMenuOpened = () => document.getElementById("side-menu").style.marginLeft === "0px";
 
-function toggleMenu() {
+const toggleMenu = () => {
     if (!isMenuOpened()) {
-        if (getWidth() < 768) {
-            openMenu();
-        } else {
-            openMenuPush();
-        }
+        (getWidth() < 768) ? openMenu() : openMenuPush();
     } else {
-        if (getWidth() < 768) {
-            closeMenu();
-        } else {
-            closeMenuPush();
-        }
+        (getWidth() < 768) ? closeMenu(): closeMenuPush();
     }
 }
 
