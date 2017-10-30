61b71d3c14913189a2edaba9e18f311ba94c1cdd
Lester Pérez Santos <lester@MacBook-Pro-de-Lester.local>
2017-10-30 23:28:04 +0100

    Added SideMenu

---
+++ components/SideMenu.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-menu id="side-menu"
+        background-color="#293352"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">
+            <i class="el-icon-view" aria-hidden="true"></i>
+            <span>Explorar</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <i class="el-icon-location-outline" aria-hidden="true"></i>
+            <span>Mapa</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+            <i class="el-icon-date" aria-hidden="true"></i>
+            <span>Calendario</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+            <i class="el-icon-bell" aria-hidden="true"></i>
+            <span>Notificaciones</span>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+  name: "side-menu"
+};
+</script>
+
+<style>
+#side-menu {
+  width: 16em;
+  padding-top: 4em;
+  position: fixed;
+  z-index: 1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
         <header-component></header-component>
+        <side-menu></side-menu>
     </div>
 </template>
 
 <script>
     import HeaderComponent from "~/components/HeaderComponent";
-
+    import SideMenu from "~/components/SideMenu";
     export default {
         data () {
             return {}
         },
-        components: { HeaderComponent }
+        components: { HeaderComponent, SideMenu}
     }
 </script>
