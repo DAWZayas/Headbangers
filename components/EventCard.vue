8ce73d6b6716832fd122f4c955b289e8f83b3fda
Droxt <droxts@gmail.com>
2017-11-07 12:16:09 +0100

    Created breakpoints.scss, finished responsive event-cards and sections

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -1,12 +1,12 @@
 <template>
-    <el-card>
+    <el-card class="event-card">
         <img class="card-img" :src="img">
-        <h3>{{title}}</h3>
+        <h3 class="event-card-title">{{title}}</h3>
         <div class="event-info">
             <el-row>
                 <el-col :span="12">
-                    <icon-text icon="lnr-calendar-full" :text="date"></icon-text>
-                    <icon-text icon="lnr-clock" :text="time"></icon-text>
+                    <icon-text class="card-data" icon="lnr-calendar-full" :text="date"></icon-text>
+                    <icon-text class="card-data" icon="lnr-clock" :text="time"></icon-text>
                 </el-col>
                 <el-col :span="12">
                     <div class="price-info">
@@ -15,7 +15,7 @@
                 </el-col>
             </el-row>
             <el-row>
-                <icon-text icon="lnr-map-marker" :text="location"></icon-text>
+                <icon-text class="card-data" icon="lnr-map-marker" :text="location"></icon-text>
             </el-row>
         </div>
         <hr>
@@ -44,7 +44,7 @@
             id: "01",
             img: "img/placeholder.png",
             title: "Concierto en nosedonde",
-            date: "01/02/2018",
+            date: "01/FEB/18",
             time: "22:00",
             location: "Sala Galileo, Madrid",
             price: "9,00",
@@ -69,37 +69,48 @@
 </script>
 
 <style lang='scss'>
+
     @import "assets/styles/colors.scss";
-    .el-card {
+    @import "assets/styles/breakpoints.scss";
+
+    .event-card {
         line-height: 2em;
-        transition: 0.3s;
-        border: 1px solid #ddd;
-    }
-    
-    .el-card h3 {
-        margin: 0;
-        text-align: center;
-    }
-    
-    .el-card:hover {
-        cursor: pointer;
-        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    }
-    
-    .el-card:hover .event-info .lnr {
-        color: $accentColor;
-    }
-    
-    .event-info .lnr {
-        transition: 0.2s;
-    }
-    
-    .el-card__body {
+        transition: box-shadow 0.3s;
+        border: 1px solid $grayLighter;
+        
+        h3 {
+            margin: 0;
+            text-align: center;
+        }   
+        
+        .lnr {
+            color: $gray;
+        }
+        .el-card__body {
         padding: 0;
+        }
     }
-    
-    .el-card .lnr {
-        color: $gray;
+
+    .event-card:hover {
+        cursor: pointer;
+            
+        .event-info {
+            
+             .lnr {
+                    color: $accentColor;
+                }
+            }
+            box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    }
+
+
+    .event-info {
+        color: $grayLight;
+        padding: 1em;
+        
+        .lnr {
+            transition: color 0.2s;
+        }
     }
     
     .card-img {
@@ -109,51 +120,99 @@
     .article-more:hover {
         background-color: $accentColorLight;
     }
-    
-    .event-info {
-        color: $lightGray;
-        padding: 1em;
-    }
-    
+
     .price-info {
-        font-size: 1.5em;
+        padding-top: .75em;
+        font-size: 2em;
         text-align: center;
-        line-height: 2.5em;
         color: $gray;
     }
     
     .event-buttons {
         display: flex;
         padding: 0.5em;
-    }
-    
-    .event-buttons>button {
-        border: none;
-        background-color: transparent;
-        cursor: pointer;
-        font-size: 0.8em;
-        font-weight: bold;
-        color: $lightGray;
-        width: 100%;
-    }
-    
-    .event-buttons>button:hover {
-        color: #409EFF;
+        
+        > button {
+            cursor: pointer;
+            width: 100%;
+            border: none;
+            background-color: transparent;
+            font-size: 0.8em;
+            font-weight: bold;
+            color: $grayLight;
+        }
+        > button:hover {
+            color: $mainColorLightest;
+        }
     }
     
     hr {
         margin: 0 0 0 5%;
         width: 90%;
+        height: .075em;
         border: 0;
-        height: 1px;
-        background: #bbb;
+        background-color: $grayLight;
     }
     
-    .liked-button .lnr {
-        color: red;
+    .liked-button {
+        .lnr {
+        color: $secondaryColor;
+        }
     }
     
-    .saved-button .lnr {
-        color: green;
+    .saved-button {
+        .lnr {
+        color: $green;
+        }
     }
+
+    @media screen and (min-width: $break-sm) and (max-width: $break-md) {
+        .event-card-title{
+            font-size: 1em;
+        }
+        .card-data{
+            font-size: .9em;
+        }
+        .price-info {
+            padding-top: 1em;
+            font-size: 1.70em;
+        }
+        .event-buttons{
+            font-size: 0.9em;
+        }
+    }
+    
+    @media screen and (min-width: $break-md) and (max-width: $break-lg) {
+        .event-card-title{
+            font-size: 1.25em;
+        }
+        .card-data{
+            font-size: 1.15em;
+        }
+        .price-info {
+            padding-top: .75em;
+            font-size: 2em;
+        }
+        .event-buttons{
+            font-size: 1.15em;
+        }
+    }
+
+    @media screen and (min-width: $break-lg) and (max-width: $break-lg-xl) {
+        .event-card-title{
+            font-size: .9em;
+        }
+        .card-data{
+            font-size: .75em;
+        }
+         .price-info {
+            padding-top: 1.25em;
+            font-size: 1.5em;
+        }
+        .event-buttons{
+            font-size: .8em;
+        }
+    }
+   
+    
 </style>
