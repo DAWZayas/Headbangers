65041f50ba905b673ddc82e00ff1794e607222bc
roselletorres <roselledecastro@yahoo.com>
2017-11-08 10:57:40 +0100

    I'm adding inputs to the Basic Form.

--- components/PublishForms/BasicsForm.vue
+++ components/PublishForms/BasicsForm.vue
@@ -1,18 +1,46 @@
 <template>
     <div>
         <h3>Basic Info</h3>
-        <el-button type="primary" @click="done">
+        <p>
+            <span>Event Name</span>
+            <el-input class="basic-form" placeholder="Event Name" v-model="input"></el-input>
+         </p>
+        
+        <div class="block">
+            <span class="basic-form">Choose Date</span>
+            <el-date-picker v-model="value1" type="date" placeholder="Pick a day"></el-date-picker>
+        </div>
+
+        <p><el-button type="primary" @click="done">
             Next
-        </el-button>
+        </el-button></p>
     </div>
 </template>
 
 <script>
     export default {
+        name: "basic-form",
+
+        data() {
+            return {
+                input: ''
+            }
+        },         
+
+        data() {
+            return {             
+               value1: ''
+            }
+        },        
+
         methods: {
             done: function() {
                 this.$emit("done")
             }
         }
-    }
+
+    };
+
+    
+
 </script>
