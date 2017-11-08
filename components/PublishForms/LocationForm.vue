5526f0359cd8f7a8e6737dcf462a2e318f85b05e
Droxt <droxts@gmail.com>
2017-11-08 15:23:58 +0100

    Made location-form and started the screen that appears when isAuth is false

--- components/PublishForms/LocationForm.vue
+++ components/PublishForms/LocationForm.vue
@@ -1,18 +1,62 @@
 <template>
     <div>
-        <h3>Location Info</h3>
-        <el-button type="primary" @click="done">
-            Next
-        </el-button>
-    </div>
-</template>
+        <h3>Location</h3>
+        <el-form ref="form" :model="form" label-width="10em">
+            <el-form-item label="Venue">
+                <el-input placeholder="where it gonna take place"></el-input>
+            </el-form-item>
 
+            <el-form-item label="Description">
+                <el-input type="textarea" placeholder="warnings, restrictions, all info you can tell us about the place"></el-input>
+            </el-form-item>
+                <el-form-item label="Street">
+                    <el-col :span="8"><el-input placeholder="'skynyrd avenue'"></el-input></el-col>
+                </el-form-item>
+                <el-form-item label="Nº">
+                    <el-col :span="4"><el-input placeholder=""></el-input></el-col>
+                </el-form-item>
+                <el-form-item label="Country">
+                    <el-col :span="12"><el-input placeholder="'north korea'"></el-input></el-col>
+                </el-form-item>
+                <el-form-item label="ZIP Code">
+                    <el-col :span="8"><el-input placeholder=""></el-input></el-col>
+                </el-form-item>
+
+            <el-form-item>
+                <el-button>Back</el-button>
+                <el-button type="primary" @click="done">Next</el-button>
+            </el-form-item>
+        </el-form>
+    </div> 
+</template>
+    
 <script>
-    export default {
-        methods: {
-            done: function() {
-                this.$emit("done")
-            }
-        }
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      }
+    };
+  },
+  methods: {
+    done: function() {
+      this.$emit("done");
     }
+  }
+};
 </script>
+<style lang="scss">
+    .location-address > {
+        margin-top: 10em;
+    }
+
+</style>
+
