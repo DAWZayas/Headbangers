367d3bf3c6ac7eac9730c58ee058e2f2b9dca872
Your Name <roselledecastro@yahoo.com>
2017-12-05 10:19:44 +0100

    Working in page Concert, downloading data from Firebase.

--- pages/concert/_id.vue
+++ pages/concert/_id.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-    <div v-if="concert" ><h1>{{concert.info.title}}</h1></div>
+        <div v-if="concert" >
+            <h1>{{concert.info.title}}</h1><br>
+            <p>assisting: {{concert.assisting}}</p>
+            <p>autor: {{concert.author}}</p>
+            <p>bands: <br>
+            <p v-for="band in concert.bands">description {{band.description}}, link {{band.link}}, name {{band.name}}</p>
+
+            <p>genres: <span v-for="genre in concert.genres">{{genre}} </span></p><br>
+            <p>{{concert.info.currency}}</p>
+            <p>{{concert.info.date}}</p>
+            <p>{{concert.info.description}}</p>
+            <p>{{concert.info.poster}}</p>
+            <p>{{concert.info.price}}</p>
+            <p>{{concert.info.time}}</p>
+            <p>{{concert.info.title}}</p><br>
+            <p>{{concert.likes}}</p><br>
+            <p>{{concert.location.city}}</p>
+            <p>{{concert.location.country}}</p>
+            <p>{{concert.location.descripton}}</p>
+            <p>{{concert.location.number}}</p>
+            <p>{{concert.location.street}}</p>
+            <p>{{concert.location.venue}}</p><br><br>
+        </div>
     </div>
 </template>
 <script>
