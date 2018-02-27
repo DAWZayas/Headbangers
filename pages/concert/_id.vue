83e7711a9e7960d19e8fd311a41c50c3a7e35862
droxt <droxt>
2018-02-27 23:54:43 +0100

    concert details - phase 1

--- pages/concert/_id.vue
+++ pages/concert/_id.vue
@@ -1,72 +1,60 @@
 <template>
-    <div class='prueba'>
-        <div v-if='concert'>
-            <div class="color">
-                <div class="cent"><div class='event-img' :style='`background-image: url(${concert.info.poster})`' alt='imagen'></div></div>
-                <div class="likes"><icon-text icon="lnr-heart" :text="concert.likes"></icon-text></div>
-                <div class='event-title no-margin text-center color'>{{concert.info.title}}</div>
-                <div class='container ma color'>
-                    <icon-text class = 'pad' icon='lnr-calendar-full' :text='formattedDate'></icon-text>
-                    <icon-text class = 'pad' icon='lnr-clock' :text='concert.info.time'></icon-text>
-                    <div class='container4 pad color'>
-                        <img class='picSize' :src="iconMoney">
-                        <icon-text icon='' :text='concert.info.price'></icon-text>
-                        <span class='pad4'> {{ concert.info.currency }}</span>
-                    </div>
+    <div class="concert-page" v-if="concert">
+        <div class="concert-img">
+            <img :src="concert.info.poster">
+        </div>
+        <div class="concert-data">
+            <div class="actions">
+                <div class="likes">
+                    <icon-text class="heart" v-if="!liked" @click.native="like" icon="lnr-heart" :text="concert.likes"></icon-text>
+                    <icon-text class="heart-filled" v-else @click.native="like" icon="lnr-heart" :text="concert.likes"></icon-text>
                 </div>
-                <icon-text class = 'pad ma adress color' icon='lnr-map-marker' :text='`${concert.location.venue} (${concert.location.number}, ${concert.location.street} - ${concert.location.city} - ${concert.location.country})`'></icon-text>
-                <div class='full-width pad ma adress color padding10' >{{ concert.location.descripton }}</div>
-            </div>
-            <div class="container0">
-                <div class="item3 container5 container6">
-                        <div class="full-width text-center item21" @click="like">
-                            <icon-text v-if="!liked" icon="lnr-heart" text="Like"></icon-text>
-                            <icon-text v-else icon="lnr-heart" class="liked-button" text="Liked"></icon-text>
-                        </div>
-                        <div class="full-width text-center item22" @click="save">
-                            <icon-text v-if="!saved" icon="lnr-bookmark" text="Save"></icon-text>
-                            <icon-text v-else icon="lnr-bookmark" class="saved-button" text="Saved"></icon-text>
-                        </div>
-                        <div class="full-width text-center item23">
-                            <icon-text icon="lnr-bubble" text="Share"></icon-text>
-                        </div>
-                        <div class="full-width text-center item24">
-                            <icon-text icon="lnr-bubble" text="Assist"></icon-text>
-                        </div>
-                    </div>
-                <div class="item0">                    
-                    <div class='pad space padding10'><span class='black'>Description: </span> {{ concert.info.description }}</div>
-                    <hr class='marg1'>
-                    <div class='container3'>
-                        <div class='pad'><span class='black'>Assisting: </span>{{concert.assisting}}</div>
-                        <div class='pad'><span class='black'>Likes: </span>{{concert.likes}}</div>
-                    </div>                    
-                    <hr class='marg1'>
-                    <div class='container2 space2 padding10'>
-                        <span class='pad black gen'>Genres: </span>
-                        <span class='pad7 pad4' v-for='genre in concert.genres' :key="genre.name"><el-tag type="info" hit="true" class="brown"> {{genre}} </el-tag></span>
-                    </div>
-                    <el-card class="box-card">
-                        <div slot="header" class="clearfix bands">
-                            <span class='bands'>Bands:</span>
-                        </div>
-                        <div v-for="band in concert.bands" :key="band.name" class="text item">
-                            <div class="board">{{band.name}} ({{band.link}})</div>
-                            <div class="board">{{band.description}}</div>   
-                        </div>                        
-                    </el-card>        
+                <div class='assisting'>
+                    <icon-text class="lighter" v-if="!assisting" @click.native="assist" icon="lnr-lighter" :text="concert.assisting"></icon-text>
+                    <icon-text class="lighter-filled" v-else @click.native="assist" icon="lnr-lighter" :text="concert.assisting"></icon-text>
                 </div>
-                <div class="item1">
-                                
+                <div class="save" @click="save">
+                    <icon-text v-if="!saved" @click.native="save" icon="lnr-bookmark" text=""></icon-text>
+                    <icon-text v-else @click.native="save" icon="lnr-bookmark" class="saved-button" text=""></icon-text>
                 </div>
-                <div class="item4">
-                    <p class='align' >Location: </p>
-                    <gmap-map :center="{lat:10, lng:10}" :zoom="7" map-type-id="terrain" style="width: 95%; height: 100%" ></gmap-map>
+                <div class="share">
+                    <icon-text icon="lnr-bubble" text=""></icon-text>
                 </div>
             </div>
+            <div class= "concert-title">
+                <h2>{{concert.info.title}}</h2>
+            </div>
+            <div class= "main-info">
+                <p>{{formattedDate + ', ' + concert.info.time}}</p>
+                <p>{{concert.info.price + ' ' + concert.info.currency}}</p>
+                <p>{{concert.location.venue + ' (' + concert.location.city + ')'}}</p>
+                <!--<span v-if="concert.location.number !== 'S/N'">{{concert.location.number}}</span>-->
+            </div>
+            <div class="description">
+                <p>{{ concert.location.descripton }}</p>
+            </div>
+            <h4 class="bands-title">Bands</h4>
+                <div class="bands" v-for="band in concert.bands" :key="band.name">
+                    <p class="band-name">{{band.name}}</p>
+                    <div class="band-body">
+                        <p class="band-description">{{band.description}}</p>
+                        <p class="band-website">Official site: <a :href="band.link">{{band.link}}</a></p> 
+                    </div>         
+                </div>
+            <h4 class="genres-title">Genres</h4>
+                <div class="genre" v-for='genre in concert.genres' :key="genre.name">
+                    <p>{{genre}}</p>
+                </div>
+            <h4 class="location-title">Location</h4>
+                <div class="location">
+                    <gmap-map :center="concert.location.coords" :zoom="8" map-type-id="terrain" style="width: 100%; height: 256px;">
+                        <gmap-marker :position="concert.location.coords" :clickable="true"></gmap-marker>
+                    </gmap-map>
+                </div>
         </div>
     </div>
 </template>
+
 <script>
 
     import firebaseApp from '~/firebaseapp';
@@ -74,35 +62,31 @@
     import {IconText} from '~/components/common';
 
     export default {
-        data () {
-            return {
-                iconMoney: '/img/icons/money.svg'
-            }
-        },
         components: {
             IconText
         },
         computed:{
-            ...mapGetters({concert: 'getConcertDetails', likedConcerts: 'getUserLiked', savedConcerts: 'getUserSaved', isAuthenticated: 'isAuthenticated'}),
+            ...mapGetters({concert: 'getConcertDetails', likedConcerts: 'getUserLiked', assistingConcerts: 'getUserAssisting', savedConcerts: 'getUserSaved', isAuthenticated: 'isAuthenticated'}),
+            
             formattedDate(){ return new Date(Number(this.concert.info.date)).toLocaleDateString()},
+            
             liked () {
                 return this.likedConcerts && Object.keys(this.likedConcerts).includes(this.id)
             },
+
+            assisting () {
+                return this.assistingConcerts && Object.keys(this.likedConcerts).includes(this.id)
+            },
+
             saved () {
                 return this.savedConcerts && Object.keys(this.savedConcerts).includes(this.id)
             },
         },        
-        mounted(){
-            this.setConcertsFullRef()
-            this.bindConcert(this.$route.params.id).then(exists => !exists && this.$router.push('/error'))
-            //firebaseApp.database().ref('/concertsFull').child(this.id).on('value',function(concert){ this.concert = concert.val() }.bind(this))
-        },
-        beforeDestroy(){
-            this.unbindConcert();
-        },
+                
         methods: {
             ...mapActions(['bindConcert', 'unbindConcert']),
             ...mapMutations(['setConcertsFullRef']),
+            
             like () {
                 if (this.isAuthenticated) {
                     !this.liked ? this.likeConcert(this.id) : this.unlikeConcert(this.id)
@@ -114,6 +98,19 @@
                     })
                 }   
             },
+
+            assist () {
+                /*if (this.isAuthenticated) {
+                    !this.assisting ? this.???(this.id) : this.???(this.id)
+                } else {
+                    this.$notify({
+                        type: 'info',
+                        message: 'You need to login',
+                        duration: 1000
+                    })
+                }*/
+            },
+
             save () {
                 if (this.isAuthenticated) {
                     !this.saved ? this.saveConcert(this.id) : this.unsaveConcert(this.id)
@@ -125,338 +122,53 @@
                     })
                 }
             }
+        },
+
+        mounted(){
+            this.setConcertsFullRef()
+            this.bindConcert(this.$route.params.id).then(exists => !exists && this.$router.push('/error'))
+            //firebaseApp.database().ref('/concertsFull').child(this.id).on('value',function(concert){ this.concert = concert.val() }.bind(this))
+        },
+        
+        beforeDestroy(){
+            this.unbindConcert();
         }
     }
 </script>
 
 <style lang='scss'>
-    @import 'assets/styles/colors.scss';
-    @import 'assets/styles/breakpoints.scss';
-        
-    .ma{
-        color: $gray;
-    }
-    .event-img{
-        padding-top: 66%;
-        background-size: cover;
-        background-position: center;
-    }
-    .event-title{
-        padding: 4% 0;
-        color: $gray;
-        font-size: 1.5em;
-        font-weight: bolder;
-        border-bottom: thin $grayLighter solid;
-    }
-    .container{
-        display: block;
-        padding-top: 2%;
-    }
-    .container2{
-        display: flex;
-        justify-content: left;
-        padding-top: 2%;
-    }
-    .container3{
-        display: flex;
-        justify-content: space-between;
-        padding-top: 2%;
-    }
-    .container4{
-        display: flex;
-        justify-content: left;
-        padding-top: 2%;
-    }
-    .container5{
-        display: flex;
-        justify-content: left;
-        padding-top: 2%;
-        padding-bottom: 2%;
-    }
-    .item4{
-            margin: 3%;
-            text-align: center;
-    }
-    .align{
-        margin-left: 3%;
-        margin-bottom: 0%;
-        text-align: left;
-        font-weight: bold;
-    }
-    .pad{
-        padding: 1% 5% 1% 5%;
-    }
-    .pad2{
-        padding-left: 10%;
-    }
-    .pad3{
-        padding-bottom: 5%;
-    }
-    .pad4{
-        padding-top: 0.7%;
-        padding-left: 2%;
-    }
-    .pad5{
-        padding-left: 30%;
-    }
-    .pad6{
-        padding-top: 5%;
-    }
-    .pad7{
-        padding: 0% 5% 1% 5%;
-    }
-    .likes{
-        margin: -3% 0 1% 1%;
-        background-color: transparent;
-    }
-    .marg1{
-        margin: 3% 5%;
-    }
-    .space{
-        padding-top: 5%;
-    }
-    .black{
-        font-weight: bold;
-    }
-    .brown{
-        background-color: $grayLight;
-        color: $grayLighter; 
-    }
-    .clearfix:before,
-    .clearfix:after {
-        display: table;
-        content: "";
-    }
-    .clearfix:after {
-        clear: both
-    }
-    .box-card {
-        width: 100%;
-        border-color: $grayLight;
-        border-style: solid;
-        background-color: $grayLight;
-        color: $grayLighter;
-    }
-    .board{
-        padding-left: 5%;
-    }
-    .bands{
-        font-weight: bold;
-    }
-    .color{
-        background-color: $mainColorLighter;
-        color: $baseColor;
-        //border-bottom: thin $gray solid;
-    }
-    .italic{
-        font-style: italic;
-        font-size: 0.7em;
-    }
-    .space2{
-        margin-bottom: 5%;
-    }
-    .gen{
-        display: none;
-    }
-    .picSize{
-        width: 1.5em;
-        height: 1.5em;
-        //background-color: pink;
-    }
-    
-    @media (min-width: $break-sm) {
-        // .prueba{
-        //     background-color:red;
-        // }
-        .event-img{
-            width: 60%;
-            margin-top:03%;
-            padding-top: 30%;
-            margin-left: 15%;
-        }        
-        .event-title{
-            padding: 2% 0;
+    @import 'assets/styles/main.scss';
+    .concert-img {        
+        img {
+            width: 100%;
         }
-        .cent{
-            background-color: $mainColor;
-        }
-        .container0{
-            display: grid;
-            grid-template-columns: 65% 35%;
-            grid-template-rows: 100%;
-        }
-        .item0 {
-            grid-column-start: 1;
-            grid-column-end: 1;
-            grid-row-start: 1;
-            grid-row-end: 3;
-        }
-        .item1 {
-            grid-column-start: 1;
-            grid-column-end: 1;
-            grid-row-start: 3;
-            grid-row-end: 3;
-            padding-top: 2%;
-            padding-bottom: 2%;
-        }
-        .item3 {
-            grid-column-start: 2;
-            grid-column-end: 2;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            padding-top: 2%;
-            padding-bottom: 2%;
-        }
-        .item4 {
-            grid-column-start: 2;
-            grid-column-end: 2;
-            grid-row-start: 2;
-            grid-row-end: 2;
-            margin: 3%;
-            //background-color:yellow;
-            text-align: center;
-        }
-        .container6 {
-            display: grid;
-            grid-template-columns: 49% 1% 49%;
-            grid-template-rows: 2fr; 
-            margin: 5% 5% 5% 5%;
-        }
-        .item21 {
-            grid-column-start: 1;
-            grid-column-end: 1;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            background-color: $yellowLighter;
-            //background-color: $accentColorDark;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-            border-radius: 5px;
-            margin: 1.5px;
-        }
-        .item22 {
-            grid-column-start: 3;
-            grid-column-end: 3;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            background-color: $yellowLighter;
-            //background-color: $mainColorLightest;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-            border-radius: 5px;
-            margin: 1.5px;
-        }
-        .item23 {
-            grid-column-start: 1;
-            grid-column-end: 1;
-            grid-row-start: 2;
-            grid-row-end: 2;
-            background-color: $yellowLighter;
-            //background-color: $mainColorLightestLight;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-            border-radius: 5px;
-            margin: 1.5px;
-        }
-        .item24 {
-            grid-column-start: 3;
-            grid-column-end: 3;
-            grid-row-start: 2;
-            grid-row-end: 2;
-            background-color: $yellowLighter;
-            //background-color: $brownLight;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-            border-radius: 5px;
-            margin: 1.5px;
-        }
-        .marg1{
-            margin: 0 5%;
-        }
-        .padding10{
-            margin-bottom: 1%;
-        }
-        .pad{
-            padding: 1% 5% 1% 5%;
-        }
-        .pad4{
-            padding-top: 2%;
-            padding-left: 8%;
-        }
-        .likes{
-        margin: -3% 0 1% 1%;
-        }
-        .box-card {
-            width: auto;
-            margin: 0% 5% 0% 5%;
-        }
-        .container{
+    }
+    .concert-data {
+        box-shadow: 0px -2px 2px rgba(0, 0, 0, .5);
+        padding: 25px;
+        .actions{
+            margin-top: -15px;
+            margin-left: -10px;
             display: flex;
-            justify-content: space-between;
-            padding-top: 0%;
+            width: 50%;
+            justify-content: space-around;
+                .lnr {
+                    cursor: pointer;
+                }
+                .lnr-heart{
+                    margin-right: 5px;
+                }
         }
-        .space2{
-            margin-bottom: 1%;
+        .concert-title {
+            text-align: center;
+            > h2 {
+                margin: 5px 0px;
+                font-size: 2.5em;
+                font-family: "Saira Extra Condensed";
+            }
         }
-        .adress{
+        .main-info {
             text-align: center;
         }
-        .gen{
-        display: inline;
-        }
     }
-
-    @media (min-width: $break-md) {
-        .container6 {
-            display: grid;
-            grid-template-columns: 24.5% 1% 24.5% 1% 24.5% 1% 24.5%;
-            grid-template-rows: 1fr; 
-            margin: 5% 5% 5% 5%;
-        }
-        .item21 {
-            grid-column-start: 1;
-            grid-column-end: 1;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            background-color: $yellowLighter;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-        }
-        .item22 {
-            grid-column-start: 3;
-            grid-column-end: 3;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            background-color: $yellowLighter;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-        }
-        .item23 {
-            grid-column-start: 5;
-            grid-column-end: 5;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            background-color: $yellowLighter;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-        }
-        .item24 {
-            grid-column-start: 7;
-            grid-column-end: 7;
-            grid-row-start: 1;
-            grid-row-end: 1;
-            background-color: $yellowLighter;
-            //border-style: solid;
-            border-width: thin;
-            padding: 8% 0 8% 0;
-        }
-    }
-
 </style>
