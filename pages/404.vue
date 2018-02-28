a0aad460617f0bca9e01892e2ed3d6b657222410
lesterbx <prz.lester@gmail.com>
2018-02-28 21:32:17 +0100

    Added 404 page

---
+++ pages/404.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="error-page">
+        <div class="text-center">
+            <span class="lnr lnr-construction"></span>
+            <h1>404</h1>
+            <h2>This page could not be found</h2>
+            <a class="link" href="/">Home page</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'empty'
+}
+</script>
+<style lang="scss">
+    @import "assets/styles/colors.scss";
+    .error-page{
+        color: #fff;
+        background-color: $mainColorLight;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100vh;
+    }  
+    .error-page .lnr{
+        font-size: 8em;
+    }  
+
+    .error-page h1{
+        margin-top: 0;
+    }
+</style>
